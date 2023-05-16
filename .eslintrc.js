8dc9f4949c9b68d7b7bd276c77ae636a5dc828f4
shubhdeep chhabra <chhabrashubhdeep@gmail.com>
2023-05-16 23:19:16 +0530

    setup eslint, prettier, husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/jsx-runtime',
+		'prettier',
+		'next/core-web-vitals',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {},
+}
