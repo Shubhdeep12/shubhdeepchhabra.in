f5dc170e692ce2c3ec7ccd36fa798713d723b018
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2023-10-28 11:42:55 +0530

    fixed nextjs version

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,44 +1,30 @@
 'use client'
 
+import Head from 'next/head'
 import Script from 'next/script'
 
-// const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
-// 	if (window !== null) {
-// 		window.gtag('config', GA_MEASUREMENT_ID, {
-// 			page_path: url,
-// 		})
-// 	}
-// }
-
 const GoogleAnalytics = () => {
-	// const pathname = usePathname()
-	// const searchParams = useSearchParams()
-
-	// useEffect(() => {
-	// 	const url = pathname + (searchParams || '').toString()
-
-	// 	pageview(process.env.GA_MEASUREMENT_ID || '', url)
-	// }, [pathname, searchParams, process.env.GA_MEASUREMENT_ID])
 	return (
 		<>
-			{console.log('set up google anayltics')}
+			<Head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+            `,
+					}}
+				/>
+			</Head>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
 			<Script
 				strategy='afterInteractive'
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-			/>
-			<Script
-				id='gtag-init'
-				strategy='afterInteractive'
-				dangerouslySetInnerHTML={{
-					__html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-                      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                      page_path: window.location.pathname,
-                      });
-                    `,
-				}}
 			/>
 		</>
 	)
