e1fb313e485c529d36e345b1ef92f13b5304244d
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-01-06 10:15:39 +0530

    update utils

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@
 import Text from '@/ui/Text'
 import BlogList from '@/components/blog/BlogList'
 import { allBlogs, Blog } from 'contentlayer/generated'
-import { getBlogGroups } from './utils'
+import { getBlogGroups } from '../../utils/blog'
 import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Blogs() {
