2a508840e42acce5853ce0759d463e8f754fe021
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2024-06-25 10:16:30 +0530

    updated packages

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,5 +1,5 @@
-import * as Sentry from '@sentry/nextjs'
-import * as Spotlight from '@spotlightjs/spotlight'
+import * as Sentry from '@sentry/nextjs';
+import * as Spotlight from '@spotlightjs/spotlight';
 Sentry.init({
 	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN, // To store dsn value in env variables and it is stored in 2 variables as on client side Nextjs needs env with prefix NEXT_PUBLIC
 
@@ -12,16 +12,17 @@ Sentry.init({
 	replaysSessionSampleRate: 0.1, // To trace 10% of sessions(without any error).
 
 	integrations: [
-		// Sentry repaly integration configs to mask all text and block media in replays captured.
+		Sentry.browserTracingIntegration(),
+		// Sentry replay integration configs to mask all text and block media in replays captured.
 		Sentry.replayIntegration({
 			maskAllText: true,
 			blockAllMedia: true,
 		}),
 	],
-})
+});
 
 if (process.env.NODE_ENV === 'development') {
 	Spotlight.init({
 		debug: true,
-	})
+	});
 }
