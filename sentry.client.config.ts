864f77263c051a49eb110572626ec83ae0fec922
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2025-04-04 13:19:52 +0530

    Update dependencies

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,23 +1,14 @@
 import * as Sentry from '@sentry/nextjs';
+
 Sentry.init({
-	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN, // To store dsn value in env variables and it is stored in 2 variables as on client side Nextjs needs env with prefix NEXT_PUBLIC
-
-	tracesSampleRate: 1, // To trace every event in production.
-
-	debug: false, // To have logs but in development environments.
-
-	replaysOnErrorSampleRate: 0.1, // To trace every session with error.
-
-	replaysSessionSampleRate: 0.1, // To trace 10% of sessions(without any error).
-
-	integrations: [
-		Sentry.browserTracingIntegration(),
-		// // Sentry replay integration configs to mask all text and block media in replays captured.
-		// new Sentry.replayIntegration({
-		// 	maskAllText: true,
-		// 	blockAllMedia: true,
-		// }),
-	],
+	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN,
+	tracesSampleRate: 1,
+	debug: false,
+	integrations: [Sentry.browserTracingIntegration()],
+	// Enable performance monitoring
+	enableTracing: true,
+	// Enable automatic instrumentation
+	autoSessionTracking: true,
 });
 
 if (process.env.NODE_ENV === 'development') {
