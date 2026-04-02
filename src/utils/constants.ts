afdaee6d9b603e2f241f244f3148bb94e071ddd8
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2026-04-02 08:00:12 +0530

    Update resume URL in constants.ts to the latest version for accurate access.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const resumeUrl = 'https://drive.google.com/file/d/18eSBaG2ysqclknHI2BxtQjwXSmu_LTpd/view?usp=sharing';
+export const resumeUrl = 'https://drive.google.com/file/d/1le6TvIqCost3l8ZJ3xSJAsKSN-lh6mQ8/view?usp=sharing';
 
 export const SKILLS = new Map([
 	[
